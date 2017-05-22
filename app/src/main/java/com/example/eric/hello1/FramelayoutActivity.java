ca148b7862c9e31e7403b91fa945c4e350e2c2b8
EricLi98 <46968083@qq.com>
2017-05-22 11:51:25 +0800

    initialize

--- app/src/main/java/com/example/eric/hello1/FramelayoutActivity.java
+++ app/src/main/java/com/example/eric/hello1/FramelayoutActivity.java
@@ -11,37 +11,37 @@ import android.widget.TextView;
 
 public class FramelayoutActivity extends AppCompatActivity {
 
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate1(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.layout_frame1);
         setContentView(R.layout.layout_frame2);
     }
 
-    protected void onCreate2(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //实例化ImageView
         ImageView imageView = new ImageView(this);
-        imageView.setImageResource(R.drawable.img_name);
-        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        imageView.setImageResource(R.drawable.img_name);//设置资源
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);//设置缩放
         imageView.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
-                FrameLayout.LayoutParams.MATCH_PARENT));
+                FrameLayout.LayoutParams.MATCH_PARENT));//设置layout_width,layout_height
+        //实例化TextView
         TextView textView1 = new TextView(this);
         textView1.setText("test");
-        textView1.setTextSize(30);
-        textView1.setGravity(Gravity.CENTER);
-        textView1.setTextColor(Color.parseColor("#f3f3f3"));
-        textView1.setTypeface(null, Typeface.BOLD);
-//        textView1.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
-//                FrameLayout.LayoutParams.MATCH_PARENT));
+        textView1.setTextSize(30);//字号
+        textView1.setGravity(Gravity.CENTER);//显示位置
+        textView1.setTextColor(Color.parseColor("#f3f3f3"));//颜色
+        textView1.setTypeface(null, Typeface.BOLD);//加粗
         FrameLayout.LayoutParams lp1 = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
-        //lp1.setMargins(0,20,0,0);
-        textView1.setLayoutParams(lp1);
-//Initializing frame layout
+        textView1.setLayoutParams(lp1);//设置layout_width,layout_height
+        //实例化布局
         FrameLayout framelayout = new FrameLayout(this);
         framelayout.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
                 FrameLayout.LayoutParams.MATCH_PARENT));
-//Adding views to FrameLayout
+        //添加子元素
         framelayout.addView(imageView);
         framelayout.addView(textView1);
+        //设置布局
         setContentView(framelayout);
     }
 
